b8cc07f0f23ddec17ee430a256ca53bdad74e8e0
Dimitrios Stathis <stathis@kth.se>
2024-05-10 16:20:05 +0200

    add enviromental var for source path

--- examples/MIMO/wrapper.py
+++ examples/MIMO/wrapper.py
@@ -1,50 +1,74 @@
 import sys
-sys.path.append('../../')
+import os
+
+SIMBRAIN_PATH = os.getenv("SIMBRAIN_PATH")
+
+sys.path.append(SIMBRAIN_PATH)
 
 from simbrain.mapping import MimoMapping
 from typing import Iterable, Optional
 import torch
+from itertools import chain
+
+# TODO this values need to be set according to the size of the crossbar and have to be the same as the C FLI interface and the VHDL
+CRB_ROW = 3
+CRB_COL = 2
+
 
 class Interface:
     # language=rst
     """
     Interface class for connecting RTL to pytorch-based simulation framework for memristor-based MIMO.
     """
-    def __init__(self, shape: Optional[Iterable[int]] = None):
-        self.shape = (1, 1) # Initialize as 1x1 memristor, in case error
-        self.shape = shape # (no_rows, no_cols)
 
+    def __init__(self, shape: Optional[Iterable[int]] = None):
+        self.shape = (CRB_ROW, CRB_COL)  # Initialize as 1x1 memristor, in case error
+        # self.shape = shape  # (no_rows, no_cols)
+        print(" Setting up crossbar, ", CRB_ROW, CRB_COL)
         # parameter setting for memristor crossbar simulation - constants
         # Memristor Array
-        memristor_structure = 'mimo'
-        memristor_device = 'new_ferro' # ideal, ferro, new_ferro, or hu
+        memristor_structure = "mimo"
+        memristor_device = "new_ferro"  # ideal, ferro, new_ferro, or hu
         c2c_variation = False
-        d2d_variation = 0 # 0: No d2d variation, 1: both, 2: Gon/Goff only, 3: nonlinearity only
+        d2d_variation = (
+            0  # 0: No d2d variation, 1: both, 2: Gon/Goff only, 3: nonlinearity only
+        )
         stuck_at_fault = False
-        retention_loss = 0 # retention loss, 0: without it, 1: during pulse, 2: no pluse for a long time
-        aging_effect = 0 # 0: No aging effect, 1: equation 1, 2: equation 2
+        retention_loss = 0  # retention loss, 0: without it, 1: during pulse, 2: no pluse for a long time
+        aging_effect = 0  # 0: No aging effect, 1: equation 1, 2: equation 2
         # Peripheral Circuit
-        wire_width = 200 # In practice, wire_width shall be set around 1/2 of the memristor size; Hu: 10um; Ferro:200nm;
+        wire_width = 200  # In practice, wire_width shall be set around 1/2 of the memristor size; Hu: 10um; Ferro:200nm;
         input_bit = 8
         CMOS_technode = 14
         ADC_precision = 32
-        ADC_setting = 4 # 2:two memristor crossbars use one ADC; 4:one memristor crossbar use one ADC
-        ADC_rounding_function = 'floor'  # floor or round
-        device_roadmap = 'HP' # HP: High Performance or LP: Low Power
+        ADC_setting = 4  # 2:two memristor crossbars use one ADC; 4:one memristor crossbar use one ADC
+        ADC_rounding_function = "floor"  # floor or round
+        device_roadmap = "HP"  # HP: High Performance or LP: Low Power
         temperature = 300
-        hardware_estimation = True # area and power estimation
+        hardware_estimation = True  # area and power estimation
 
-        self.sim_params = {'device_structure': memristor_structure, 'device_name': memristor_device,
-                      'c2c_variation': c2c_variation, 'd2d_variation': d2d_variation,
-                      'stuck_at_fault': stuck_at_fault, 'retention_loss': retention_loss,
-                      'aging_effect': aging_effect, 'wire_width': wire_width, 'input_bit': input_bit,
-                      'batch_interval': 1, 'CMOS_technode': CMOS_technode, 'ADC_precision': ADC_precision,
-                      'ADC_setting': ADC_setting, 'ADC_rounding_function': ADC_rounding_function,
-                      'device_roadmap': device_roadmap, 'temperature': temperature,
-                      'hardware_estimation': hardware_estimation}
+        self.sim_params = {
+            "device_structure": memristor_structure,
+            "device_name": memristor_device,
+            "c2c_variation": c2c_variation,
+            "d2d_variation": d2d_variation,
+            "stuck_at_fault": stuck_at_fault,
+            "retention_loss": retention_loss,
+            "aging_effect": aging_effect,
+            "wire_width": wire_width,
+            "input_bit": input_bit,
+            "batch_interval": 1,
+            "CMOS_technode": CMOS_technode,
+            "ADC_precision": ADC_precision,
+            "ADC_setting": ADC_setting,
+            "ADC_rounding_function": ADC_rounding_function,
+            "device_roadmap": device_roadmap,
+            "temperature": temperature,
+            "hardware_estimation": hardware_estimation,
+        }
 
         # MimoMaping Initialization
-        device = 'cpu'
+        device = "cpu"
         batch_size = 1
         self.interface = MimoMapping(sim_params=self.sim_params, shape=self.shape)
         self.interface.to(device)
@@ -69,18 +93,32 @@ interface_instance = Interface()
 def mem_program(value):
     # array to tensor
     matrix_tensor = torch.tensor(value).unsqueeze(0)
-
     return interface_instance.program(matrix_tensor)
+    # return value + 1
 
 
 def mem_compute(value):
     # array to tensor
     vector_tensor = torch.tensor(value).unsqueeze(0).unsqueeze(0)
-
+    # print("hello", vector_tensor)
     output_tensor = interface_instance.compute(vector_tensor)
-    output_tensor = output_tensor.squeezze(0).squeeze(0)
-
+    # print(output_tensor)
+    output_list = output_tensor.tolist()
+    # print(output_list)
+    output_flatten = list(chain.from_iterable(output_list))
+    output_flatten = list(chain.from_iterable(output_flatten))
+    # print(output_flatten)
     # tensor to array
-    output = output_tensor.numpy()
+    output = output_flatten  # output_tensor.numpy()
     return output
 
+
+def main():
+
+    print(SIMBRAIN_PATH)
+    x = mem_program([[0, 0.1], [0.1, 0.2], [0.2, 0.3]])
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
